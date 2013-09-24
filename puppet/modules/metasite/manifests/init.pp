c1821cb3190822b14e49e52cc7668736353859e5
Guy John <guy@rumblesan.com>
2013-09-24 22:03:10 +0100

    Adding manifest for metasite config

---
+++ puppet/modules/metasite/manifests/init.pp
@@ -0,0 +1,29 @@
+
+class metasite {
+
+    $metasite_user  = 'meta'
+    $metasite_group = 'meta'
+
+    $www_root       = '/var/www'
+
+    $metasite_www   = "${www_root}/metapiano"
+    $metasite_logs  = '/var/log/metapiano'
+
+    $metasite_host  = 'metapiano.com'
+
+    userconfig { $metasite_user:
+        username  => $metasite_user,
+        uid       => 2001,
+        groupname => $metasite_group,
+        gid      => 3001
+    }
+
+    file { [ $www_root, $metasite_www, $metasite_logs ]:
+        ensure  => 'directory',
+        owner   => $metasite_user,
+        group   => $metasite_group,
+        require => Userconfig[$metasite_user]
+    }
+
+}
+
