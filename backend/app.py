d93534558222e5a215cc40939970d78533a411f5
Guy John <guy@rumblesan.com>
2013-09-25 23:07:15 +0100

    Service sends back json

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 
-from flask import Flask
+from flask import Flask, Response
 
 from werkzeug.contrib.fixers import ProxyFix
 
 import os
+import json
 
 app = Flask('metapiano')
 config = os.path.join(app.root_path, 'config', 'production.cfg')
@@ -15,9 +16,17 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 app.debug = True
 
 
+def data_response(data):
+    return Response(
+        json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+
+
 @app.route('/service/api', methods=['GET'])
 def main():
-    return "hello\n"
+    return data_response({'message': 'no data'})
 
 if __name__ == '__main__':
     app.run()
