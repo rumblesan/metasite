c2f079941acbd726891452bca39a8241c529f10d
Guy John <guy@rumblesan.com>
2013-09-25 23:01:31 +0100

    Adding config file in

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,14 @@ from flask import Flask
 
 from werkzeug.contrib.fixers import ProxyFix
 
+import os
+
 app = Flask('metapiano')
+config = os.path.join(app.root_path, 'config', 'production.cfg')
+app.config.from_pyfile(config)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
 
 app.debug = True
 
