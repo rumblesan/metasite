a0814f88e385cde3309e881f9784b622c387cf13
Guy John <guy@rumblesan.com>
2013-09-30 12:28:53 +0100

    updating install script

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+sudo apt-get update
+
 ln -s /opt/metasite/puppet /etc/puppet
+
 puppet apply --modulepath /etc/puppet/modules /etc/puppet/manifests/site.pp
